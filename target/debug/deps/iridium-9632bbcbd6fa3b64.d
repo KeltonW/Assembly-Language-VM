42f0bde3b5f54995ac278385e2378a5e2945124a
Kel <keltonwambu@gmail.com>
2025-05-15 19:44:19 +0100

    Delete target directory

--- target/debug/deps/iridium-9632bbcbd6fa3b64.d
+++
@@ -1,7 +0,0 @@
-C:\Users\Kelton\OneDrive\Desktop\Coding Stuff\My Codes\Rust\iridium\target\debug\deps\iridium-9632bbcbd6fa3b64.exe: src\main.rs src\instruction.rs src\vm.rs
-
-C:\Users\Kelton\OneDrive\Desktop\Coding Stuff\My Codes\Rust\iridium\target\debug\deps\iridium-9632bbcbd6fa3b64.d: src\main.rs src\instruction.rs src\vm.rs
-
-src\main.rs:
-src\instruction.rs:
-src\vm.rs:
--- target/debug/deps/iridium.d
+++
@@ -1,7 +0,0 @@
-C:\Users\Kelton\OneDrive\Desktop\Coding Stuff\My Codes\Rust\iridium\target\debug\deps\iridium.exe: src\main.rs src\instruction.rs src\vm.rs
-
-C:\Users\Kelton\OneDrive\Desktop\Coding Stuff\My Codes\Rust\iridium\target\debug\deps\iridium.d: src\main.rs src\instruction.rs src\vm.rs
-
-src\main.rs:
-src\instruction.rs:
-src\vm.rs:
